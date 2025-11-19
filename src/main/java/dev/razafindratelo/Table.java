40270d114a47bc2059646b5c39aecf45324a7bf6
Abega1642 <hei.abega.2@gmail.com>
2025-11-19 09:09:19 +0300

    feat: add Table class

--- src/main/java/dev/razafindratelo/Table.java
+++ src/main/java/dev/razafindratelo/Table.java
@@ -1,4 +1,19 @@
 package dev.razafindratelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Table {
+  private double length;
+  private double width;
+  private String state;
+  private String color;
+
+  public double getArea() {
+    return length * width;
+  }
 }
--- src/test/java/dev/razafindratelo/TableTest.java
+++ src/test/java/dev/razafindratelo/TableTest.java
@@ -1,6 +1,19 @@
 package dev.razafindratelo;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class TableTest {
-  
+  @Test
+  void should_get_zero_with_getArea() {
+    var table = new Table(0, 0, "not existing", null);
+    assertEquals(0, table.getArea());
+  }
+
+  @Test
+  void should_get_10_with_getArea() {
+    var table = new Table(5, 2, "small", "black");
+    assertEquals(10, table.getArea());
+  }
 }
